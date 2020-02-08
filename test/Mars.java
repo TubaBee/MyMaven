fac5dc399553e919f8e8bdf7db2f27b504c401a5
tubaBee <tuba.onslbee@gmail.com>
2020-02-07 21:58:08 -0500

    Gite tekrar eklendi ve dosyalar tekrar eklendi

---
+++ test/Mars.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Mars {
+    public static void main(String[] args){
+  System.out.println("Hello world");
+  System.out.println("\n");
+  
+  
+  System.out.println("Hello world");
+      System.out.println("Gitten ekleyip eclipse gondericez");
+
+}
+}
